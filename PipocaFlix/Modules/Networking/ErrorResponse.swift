2eb79eb2088b1cfb6926b47126ad84b0cda69f60
Ranieri Aguiar <ranieri72@hotmail.com>
2022-10-25 20:08:04 -0300

    Create Networking module

---
+++ PipocaFlix/Modules/Networking/ErrorResponse.swift
@@ -0,0 +1,24 @@
+//
+//  ErrorResponse.swift
+//  PipocaFlix
+//
+//  Created by Ranieri Aguiar on 25/10/22.
+//
+
+enum ErrorResponse: String {
+    case apiError
+    case invalidEndpoint
+    case invalidResponse
+    case noData
+    case serializationError
+    
+    public var description: String {
+        switch self {
+        case .apiError: return "Ooops, there is something problem with the api"
+        case .invalidEndpoint: return "Ooops, there is something problem with the endpoint"
+        case .invalidResponse: return "Ooops, there is something problem with the response"
+        case .noData: return "Ooops, there is something problem with the data"
+        case .serializationError: return "Ooops, there is something problem with the serialization process"
+        }
+    }
+}
